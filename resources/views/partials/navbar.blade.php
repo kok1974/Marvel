b47634aa0f7f3c3489451bcc23f801434c7f9f88
alumno <alumno@vm1>
2021-05-29 22:47:26 +0200

    cosillas1

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,31 @@
+<div class="row mx-0">
+    <nav class="navbar navbar-expand-sm colorNav">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item mx-5">
+                    <a class="nav-link text-white" href="#personajes">Personajes</a>
+                </li>
+                <li class="nav-item dropdown mx-5">
+                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Comics
+                    </a>
+                    <div class="dropdown-menu colorNav" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item text-white colorhover" href="#">Por series</a>
+                      <a class="dropdown-item text-white" href="#">Por personajes</a>
+                      <a class="dropdown-item text-white" href="#">Por eventos</a>
+                      <a class="dropdown-item text-white" href="#">Por autor</a>
+                    </div>
+                  </li>
+                <li class="nav-item mx-5">
+                    <a class="nav-link text-white" href="#coleccion">Tu coleccion</a>
+                </li>
+                <li class="nav-item mx-5">
+                    <a class="nav-link text-white" href="#administracion">Administracion</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
